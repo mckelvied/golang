607d3eaa769c7b04a930bebd7ff420f1469fc517
mckelvied <david.mckelvie@yahoo.com>
2026-08-25 23:04:40 -0700

    save to file

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 type deck []string
 
@@ -23,4 +27,25 @@ func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	return deck(strings.Split(string(bs), ","))
 }
--- cards/main.go
+++ cards/main.go
@@ -1,6 +1,31 @@
 package main
 
+import "fmt"
+
 func main() {
+	fmt.Println()
+
 	cards := newDeck()
 	cards.print()
+	hand, remainingCards := deal(cards, 5)
+	fmt.Println("Hand:")
+	hand.print()
+	fmt.Println("Remaining Cards:")
+	remainingCards.print()
+
+	fmt.Println("--------------------")
+	fmt.Println("Deck as String:")
+	fmt.Println(cards.toString())
+
+	err := cards.saveToFile("deck.txt")
+	if err != nil {
+		fmt.Println("Error saving deck to file:", err)
+	} else {
+		fmt.Println("Deck saved to deck.txt")
+	}
+
+	fmt.Println("--------------------")
+	newCards := newDeckFromFile("deck.txt")
+	fmt.Println("New Deck from File:")
+	newCards.print()
 }
