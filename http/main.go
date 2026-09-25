2d6a7289b245cce87431a842490f6b0c51d6f2d8
mckelvied <david.mckelvie@yahoo.com>
2026-09-25 16:56:21 -0700

    http

--- http/main.go
+++ http/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -14,13 +17,21 @@ func main() {
 	defer resp.Body.Close()
 
 	// Process the response...
-	bs := make([]byte, 99999)
-	n, err := resp.Body.Read(bs)
-	if err != nil {
-		fmt.Println("Error 2:", err)
-		return
-	}
-	fmt.Println("Read bytes:", n)
-	fmt.Println(string(bs[:n]))
+	// bs := make([]byte, 99999)
+	// n, err := resp.Body.Read(bs)
+	// if err != nil {
+	// 	fmt.Println("Error 2:", err)
+	// 	return
+	// }
+	// fmt.Println("Read bytes:", n)
+	// fmt.Println(string(bs[:n]))
 
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
 }
